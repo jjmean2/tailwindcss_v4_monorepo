aa7d13cb0a6b70ddc165f89eff7feef8fb42dda9
Jongwan Lee <jjmean2@gmail.com>
2025-02-16 21:52:06 +0900

    feat: add my-next-app package

---
+++ packages/design/example/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "design/components/ui/button";
+
+function App() {
+  return (
+    <div className="bg-red-300">
+      Hello World
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
